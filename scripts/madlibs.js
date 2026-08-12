f7acdeedf1fd09ca215126bde88b1bdaba3aec3a
edwardcoded77 <eolagunju25@email.com>
2026-08-12 17:44:35 -0400

    add a closing

--- scripts/madlibs.js
+++ scripts/madlibs.js
@@ -1,3 +1,5 @@
+
+/* 
 // User Input
 let name = prompt("A name:");
 let adjective = prompt("An adjective:");
@@ -15,10 +17,60 @@ makeCard();
 
 
 
-// function makeGreeting
+ function makeGreeting
 
 function makeGreeting(){
     console.log("Hello everyone, my name is " + name );
 }
 
-makeGreeting()
+makeGreeting()  
+*/
+
+
+
+/*========================================================= */
+/*
+let name = prompt("A name:");
+let adjective = prompt("An adjective:");
+let noun = prompt("A noun:");
+
+
+let closings = [
+  "You are a legend.",
+  "Stay golden.",
+  "Signs point to awesome.",
+];
+
+function randomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+
+function makeCard() {
+  console.log("Dear " + name + ",");
+  console.log("You are as " + adjective + " as a " + noun + ".");
+  let index = randomNumber(0, closings.length - 1);
+  console.log(closings[index]);
+}
+
+makeCard();
+
+
+============================================== */
+
+let style = prompt("Fancy or Silly ?");
+let name = prompt("A name:");
+
+function makeCard(){
+  if (style.toLowerCase() === "fancy"){
+   console.log("Dearest " + name + ",");
+   } 
+   else {
+      console.log("YO " + name + "!");
+   }
+
+
+  // rest of the card unchanged 
+   console.log("You are awseome!.");
+}
+
+makeCard();
