92311fd92f83f79367fbcff4f86210108032b9b7
edwardcoded77 <eolagunju25@email.com>
2026-08-14 16:52:10 -0400

    Random closing on the page closes #4

--- scripts/card.js
+++ scripts/card.js
@@ -2,10 +2,28 @@ let makeButton = document.getElementById("make-button");
 let nameInput  = document.getElementById("name-input");
 let adjectiveInput = document.getElementById("adjective-input");
 let nounInput = document.getElementById("noun-input");
+let cardText = document.getElementById("card-text");
+let closeSpace = document.getElementById("closing-text")
+
+let closings = [
+  "The race to victory starts now.",
+  "Prepare yourself for the battle ahead.",
+  "Failure is not an option.",
+  "You are on the winning side.",
+  "Let the drums roll!"
+];
+
+function randomNumber(a, b){
+  return Math.floor(Math.random() * (b-a+1) + a );
+}
 
 function makeCard(){
-alert("click");
+// alert("click");
+let name = nameInput.value;
+let adjective = adjectiveInput.value;
+let noun = nounInput.value;
+cardText.textContent = "Dear " + name + " , you are as " + adjective + " as a " + noun + ". Happy birthday from the generator. "
+closeSpace.textContent = closings[randomNumber(0 , 4)];
 }
 
 makeButton.addEventListener("click", makeCard);
-ansSpace.textContent = choices[randomNumber(0 , 4)];
